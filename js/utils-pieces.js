d08ca2233f16bcd2daeba90e69ca8f5400eb9dcf
biologicmachine-lab <bio.logic.machine@gmail.com>
2026-08-26 00:31:05 -0400

    Add unicode pieces and files for chess app

---
+++ js/utils-pieces.js
@@ -0,0 +1,10 @@
+// js/utils-pieces.js
+window.App = window.App || {};
+
+App.Pieces = {
+  UNICODE: {
+    w: { p:'♙', n:'♘', b:'♗', r:'♖', q:'♕', k:'♔' },
+    b: { p:'♟', n:'♞', b:'♝', r:'♜', q:'♛', k:'♚' }
+  },
+  FILES: ['a','b','c','d','e','f','g','h']
+};
